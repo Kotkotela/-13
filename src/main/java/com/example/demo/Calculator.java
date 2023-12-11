b88d263c2b064b52746eed1cf32e09315b4d762c
Kotkotela <boss.nardos@mail.ru>
2023-12-11 19:03:24 +0300

    задание 13

---
+++ src/main/java/com/example/demo/Calculator.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Calculator {
+
+    @Qualifier("subtractor")
+    @Autowired
+    Operation operation;
+
+    public void calc(double a, double b) {
+        System.out.println(operation.getResult(a, b));
+    }
+}
